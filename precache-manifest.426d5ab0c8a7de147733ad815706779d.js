e493f54ea96660a52db7b65687cb969b671af823
Mark Graham <markgr26@googlemail.com>
2020-09-21 21:57:31 +0100

    Updates

---
+++ precache-manifest.426d5ab0c8a7de147733ad815706779d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc701b9747ffb3d1856ec7b817800fd0",
+    "url": "/touch/index.html"
+  },
+  {
+    "revision": "c64a61cd4cd69291143c",
+    "url": "/touch/static/css/main.687a10c6.chunk.css"
+  },
+  {
+    "revision": "4d1313fb69d513245f90",
+    "url": "/touch/static/js/2.3389960c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/touch/static/js/2.3389960c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c64a61cd4cd69291143c",
+    "url": "/touch/static/js/main.f4314aa1.chunk.js"
+  },
+  {
+    "revision": "495f032aa7f2cae44356",
+    "url": "/touch/static/js/runtime-main.3262c82a.js"
+  }
+]);
